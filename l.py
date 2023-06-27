f3edbc1948f779bb080a55d0c333efc070af5230
HpDima <136693475+HpDima@users.noreply.github.com>
2023-06-27 16:00:42 +0300

    Update l.html

--- l.py
+++ l.py
@@ -1,19 +1,35 @@
-def erer(d, q):
-    fed = set(a)
-    for c in d:
-        if c in fed:
-             q += 1
-    if len(a) % 2 != 0:
-        return False
-    elif q == s:
-        return True
-    else:
-        return False
-q = 0
-d = []
-a = input()
-s = len(a) // 2
-for i in range(s):
-    k = a[i]
-    d.append(k)
-print(erer(d, q))
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Первый сайт</title>
+    <link rel="stylesheet" href="rgf.css">
+</head>
+<body>
+<h1> Заголовок 1</h1>
+<h2> Заголовок 1</h2>
+<h3> Заголовок 1</h3>
+<h4> Заголовок 1</h4>
+<h5> Заголовок 1</h5>
+<p> "Я помню чудное
+    мгновение передо мной
+    явилась ты
+</p>
+<span> я еее</span>
+<span> я еее</span>
+<ul>
+    <li>Дима</li>
+    <li>Маша</li>
+    <li>Марк</li>
+</ul>
+<ol>
+    <li>Дима</li>
+    <li>Маша</li>
+    <li>Марк</li>
+</ol>
+<img src="image.jpg">
+</body>
+</html>
