6d64821d2f59ec5e08d1f56b889f9731588afc3c
HpDima <136693475+HpDima@users.noreply.github.com>
2023-06-28 11:36:21 +0300

    Updated.css

--- l.py
+++ l.py
@@ -1,35 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Первый сайт</title>
-    <link rel="stylesheet" href="rgf.css">
-</head>
-<body>
-<h1> Заголовок 1</h1>
-<h2> Заголовок 1</h2>
-<h3> Заголовок 1</h3>
-<h4> Заголовок 1</h4>
-<h5> Заголовок 1</h5>
-<p> "Я помню чудное
-    мгновение передо мной
-    явилась ты
-</p>
-<span> я еее</span>
-<span> я еее</span>
-<ul>
-    <li>Дима</li>
-    <li>Маша</li>
-    <li>Марк</li>
-</ul>
-<ol>
-    <li>Дима</li>
-    <li>Маша</li>
-    <li>Марк</li>
-</ol>
-<img src="image.jpg">
-</body>
-</html>
+.paragraf-6 {
+    font-size: 24pt;
+    margin-left: auto;
+    margin-right: auto;
+    width: 6em;
+    font-style: italic;
+    text-shadow: 1px 1px 2px black, 0 0 1em green;
+}
